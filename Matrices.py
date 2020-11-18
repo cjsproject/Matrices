183b34f9e3382f7b58d132e320b599e920780e92
cucumberhands <cyrus.simons@yahoo.com>
2020-11-17 19:30:29 -0500

    elementary row ops

---
+++ Matrices.py
@@ -0,0 +1,55 @@
+class Matrix:
+    def __init__(self):
+        self.row = 0
+        self.col = 0
+        self.mat_arr = []
+
+    def __init__(self, n, m, arr):
+        temp_mat = []
+        temp_row = []
+        #number check
+        for row in arr:
+            for e in row:
+                try:
+                    temp_row.append(float(e))
+                except:
+                    print("Matrix must contain Numbers.")
+                    exit()
+            temp_mat.append(temp_row)
+            temp_row = []
+        self.row = n
+        self.col = m
+        self.mat_arr = temp_mat
+
+    def add(self, row_a, row_b, mult=1):
+        temp = []
+        i = 0
+        for x in self.mat_arr[row_a]:
+            a = x + mult*self.mat_arr[row_b][i]
+            temp.append(a)
+            i += 1
+        self.mat_arr[row_a] = temp
+
+        return Matrix(self.row, self.col, self.mat_arr)
+
+
+    def mult(self, row, const):
+        temp = []
+        for e in self.mat_arr[row]:
+            a = e * const
+            temp.append(a)
+        self.mat_arr[row] = temp
+
+        return Matrix(self.row, self.col, self.mat_arr)
+
+
+    def swap(self, row_a, row_b):
+        temp = self.mat_arr[row_a]
+        self.mat_arr[row_a] = self.mat_arr[row_b]
+        self.mat_arr[row_b] = temp
+
+        return Matrix(self.row, self.col, self.mat_arr)
+
+
+    def __str__(self):
+        return str(self.mat_arr)
---
+++ main.py
@@ -0,0 +1,29 @@
+from Matrices import Matrix
+from copy import deepcopy
+
+if __name__ == '__main__':
+    # Construct initial arrayMatrix
+    A = [[8, 2],
+         [5, 6]]
+    print(A)
+
+    # Create Matrix object
+    B = Matrix(len(A), len(A), A)
+    print(B)
+
+    """Elementary Row Operations"""
+
+    # Add two rows
+    B.add(0, 1, mult=-1)
+    print(B)
+
+    # Multiply a row by a constant
+    B.mult(0, 2)
+    print(B)
+
+    D = deepcopy(B)
+
+    # Swap rows
+    C = B.swap(0, 1)
+    print(B, C, D, sep='\n')
+
