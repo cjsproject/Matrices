d6e1492df53689e5aabd56925aea97158debc418
cucumberhands <cyrus.simons@yahoo.com>
2020-11-18 16:11:15 -0500

    added multiplication of nxm matrices

--- Matrices.py
+++ Matrices.py
@@ -1,39 +1,39 @@
 from fractions import Fraction
 
+
 class Matrix:
     def __init__(self):
         self.row = 0
         self.col = 0
         self.mat_arr = []
 
-    def __init__(self, n, m, arr):
+    def __init__(self, arr):
         temp_mat = []
         temp_row = []
-        #number check
+        # number check
         for row in arr:
             for e in row:
                 try:
                     temp_row.append(float(e))
                 except:
-                    print("Matrix must contain Numbers.")
+                    raise Exception("Matrix must contain Numbers.")
                     exit()
             temp_mat.append(temp_row)
             temp_row = []
-        self.row = n
-        self.col = m
+        self.row = len(arr)
+        self.col = len(arr[0])
         self.mat_arr = temp_mat
 
     def add(self, row_a, row_b, mult=1):
         temp = []
         i = 0
         for x in self.mat_arr[row_a]:
-            a = x + mult*self.mat_arr[row_b][i]
+            a = x + mult * self.mat_arr[row_b][i]
             temp.append(a)
             i += 1
         self.mat_arr[row_a] = temp
 
-        return Matrix(self.row, self.col, self.mat_arr)
-
+        return Matrix(self.mat_arr)
 
     def mult(self, row, const):
         temp = []
@@ -42,16 +42,29 @@ class Matrix:
             temp.append(a)
         self.mat_arr[row] = temp
 
-        return Matrix(self.row, self.col, self.mat_arr)
+        return Matrix(self.mat_arr)
 
+    def multMat(self, matrix_b):
+        if isinstance(matrix_b, Matrix) and self.col == matrix_b.row:
+            temp_mat = []
+            mat_a = self.mat_arr
+            mat_b = matrix_b.mat_arr
+            result = [[sum(a*b for a, b in zip(A_row, B_col)) for B_col in zip(*mat_b)] for A_row in mat_a]
+
+
+        else:
+            raise Exception("must be of type matrix")
+
+        self.mat_arr = result
+        self.__str__()
+        return Matrix(self.mat_arr)
 
     def swap(self, row_a, row_b):
         temp = self.mat_arr[row_a]
         self.mat_arr[row_a] = self.mat_arr[row_b]
         self.mat_arr[row_b] = temp
 
-        return Matrix(self.row, self.col, self.mat_arr)
-
+        return Matrix(self.mat_arr)
 
     def __str__(self):
         output = ""
--- main.py
+++ main.py
@@ -2,28 +2,36 @@ from Matrices import Matrix
 from copy import deepcopy
 
 if __name__ == '__main__':
-    # Construct initial arrayMatrix
-    A = [[8, 2],
-         [5, 6]]
-    print(A)
+    # Construct initial array
+    arr_a = [[1, 2],
+             [5, 6]]
+    print(arr_a)
+
+    arr_z = [[1, 2, 8],
+             [5, 6, 2]]
 
     # Create Matrix object
-    B = Matrix(len(A), len(A), A)
-    print(B)
+    Z = Matrix(arr_z)
+    A = Matrix(arr_a)
+    print(A, Z, sep='\n')
 
     """Elementary Row Operations"""
 
     # Add two rows
-    B.add(0, 1, mult=-1)
-    print(B)
+    A.add(0, 1, mult=-1)
+    print(A)
 
     # Multiply a row by a constant
-    B.mult(0, 0.25)
-    print(B)
+    A.mult(0, 0.25)
+    print(A)
 
-    D = deepcopy(B)
+    D = deepcopy(A)
 
     # Swap rows
-    C = B.swap(0, 1)
-    print(B, C, D, sep='\n')
+    C = A.swap(0, 1)
+    C.swap(0, 1)
+    print(A, C, D, sep='\n')
 
+    A.multMat(Z)
+
+    print("A*Z: ", A, sep='\n')
